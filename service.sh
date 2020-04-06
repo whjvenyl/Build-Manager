504702378489938e2faa5d25c5484a1b95d56e7b
pmspr4100 <49132275+pmspr4100@users.noreply.github.com>
2020-04-06 05:29:51 +0200

    v1.4

--- service.sh
+++ service.sh
@@ -1,4 +1,4 @@
-# Virtual memory tweaks
+# Virtual memory tweaks / internet speed tweaks
 stop perfd
 echo '5' > /proc/sys/vm/swappiness
 echo '0' > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
@@ -8,8 +8,24 @@ echo '128' > /sys/block/mmcblk0/queue/read_ahead_kb
 echo '128' > /sys/block/mmcblk1/queue/read_ahead_kb
 echo '4096' > /proc/sys/vm/min_free_kbytes
 echo '0' > /proc/sys/vm/oom_kill_allocating_task
+echo "0" > /proc/sys/vm/panic_on_oom;
 echo '5' > /proc/sys/vm/dirty_ratio
 echo '20' > /proc/sys/vm/dirty_background_ratio
+echo "0" > /proc/sys/net/ipv4/tcp_timestamps;
+echo "0" > /proc/sys/vm/laptop_mode;
+echo "1" > /proc/sys/net/ipv4/tcp_tw_reuse;
+echo "1" > /proc/sys/net/ipv4/tcp_sack;
+echo "1" > /proc/sys/net/ipv4/tcp_tw_recycle;
+echo "1" > /proc/sys/net/ipv4/tcp_window_scaling;
+echo "5" > /proc/sys/net/ipv4/tcp_keepalive_probes;
+echo "30" > /proc/sys/net/ipv4/tcp_keepalive_intvl;
+echo "30" > /proc/sys/net/ipv4/tcp_fin_timeout;
+echo "404480" > /proc/sys/net/core/wmem_max;
+echo "404480" > /proc/sys/net/core/rmem_max;
+echo "256960" > /proc/sys/net/core/rmem_default;
+echo "256960" > /proc/sys/net/core/wmem_default;
+echo "4096,16384,404480" > /proc/sys/net/ipv4/tcp_wmem;
+echo "4096,87380,404480" > /proc/sys/net/ipv4/tcp_rmem;
 sleep 30
 chmod 666 /sys/module/lowmemorykiller/parameters/minfree
 chown root /sys/module/lowmemorykiller/parameters/minfree
